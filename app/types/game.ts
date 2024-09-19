d46b5177871da3dd1eec3f80a453dfdedc9eec07
Rafael Souza <rafaelsouza@cogmonkeys.com>
2024-09-19 00:11:11 -0300

    created nextapp

---
+++ app/types/game.ts
@@ -0,0 +1,11 @@
+interface Game {
+    id: number;
+    team1: string;
+    team2: string;
+    time: string;
+    championship: string;
+    stadium: string;
+    broadcast: string;
+  }
+  
+  export default Game;
